f912dd25d127067dbdff520942690c04be9730fd
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-08-20 10:29:00 -0300

    refactoring api_test.go

--- api_test.go
+++ api_test.go
@@ -7,28 +7,47 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateDeck(t *testing.T) {
-	router := setupRouter()
+  response := callApi(http.MethodPost, "/api/deck/new", `{"shuffled": false}`)
 
-	w := httptest.NewRecorder()
+	assert.Equal(t, http.StatusOK, response.Code)
 
-	jsonBody := []byte(`{"shuffled": false}`)
-	bodyReader := bytes.NewReader(jsonBody)
-
-	req, _ := http.NewRequest(http.MethodPost, "/api/deck/new", bodyReader)
-
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusOK, w.Code)
-
-	jsonStr := w.Body.String()
-	bodyRes := map[string]interface{}{}
-	json.Unmarshal([]byte(jsonStr), &bodyRes)
+  bodyRes := convertBodyToMap(response.Body.String())
 
 	assert.NotNil(t, bodyRes["deck_id"])
 	assert.Equal(t, false, bodyRes["shuffled"])
 	assert.Equal(t, float64(52), bodyRes["remaining"])
+}
+ 
+func setupTestApi() (*gin.Engine, *httptest.ResponseRecorder) {
+  router := setupRouter()
+	w := httptest.NewRecorder()
+
+  return router, w
+}
+
+func makeBodyReq(param string) *bytes.Reader {
+	return bytes.NewReader([]byte(param))
+}
+
+func callApi(method, uri, body string) *httptest.ResponseRecorder {
+	router, responseRecorder := setupTestApi()
+
+	bodyReader := makeBodyReq(`{"shuffled": false}`)
+	req, _ := http.NewRequest(method, "/api/deck/new", bodyReader)
+
+	router.ServeHTTP(responseRecorder, req)
+
+  return responseRecorder
+}
+
+func convertBodyToMap(bodyString string) map[string]interface{} {
+	bodyRes := map[string]interface{}{}
+	json.Unmarshal([]byte(bodyString), &bodyRes)
+
+  return bodyRes
 }
